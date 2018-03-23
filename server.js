52b0ed14c90d086f30a9aa9573156f3cc38a4494
codemode9 <timourbasegmez@hotmail.com>
2018-03-23 11:43:08 -0400

    boiler server

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+// set up express app
+var app = require("./models");
+//require models for syncing
+var db = require("./models");
+
+// set up express to handle data parsing
+
+//parse application
+app.use(bodyParser.urlencoded({ extended: true}));
+//parse app/json
+app.use(bodyParser.json());
+
+//static directory
+app.use(express.static("public"));
+
+//routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+//syncing models and starting express
+db.sequelize.sync({ force: true}).then(function() {
+    app.listen(PORT, function() {
+        console.log(`App listening on PORT ${PORT}.`);
+    });
+});
