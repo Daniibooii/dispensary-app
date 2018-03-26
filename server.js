b171ad0630d6f021d7ff60e05654018f1374c4df
evangelosv93 <evan7@umbc.edu>
2018-03-26 10:57:54 -0400

    second

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+const path = require('path');
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  return res.render('index', {});
+});
+
+
+
+
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log('Relative path', path.join(__dirname, 'public'));
+  console.log(`Server listening on: http://localhost: ${PORT}`);
+});
