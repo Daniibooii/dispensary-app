6f28e5b9c0a24d215464f5621a733aab292dd512
unknown <gw-danmace@hotmail.com>
2018-03-26 07:59:11 -0400

    better api routes template added

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -13,78 +13,41 @@
 // template below //
 // Requiring our Todo model
 var db = require("../models");
+var passport = require("../config/passport");
 
-// Routes
-// =============================================================
 module.exports = function(app) {
-
-  // GET route for getting all of the posts
-  app.get("/api/posts/", function(req, res) {
-    db.Post.findAll({})
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
+  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+    res.json("/members");
   });
 
-  // Get route for returning posts of a specific category
-  app.get("/api/posts/category/:category", function(req, res) {
-    db.Post.findAll({
-      where: {
-        category: req.params.category
-      }
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
-
-  // Get rotue for retrieving a single post
-  app.get("/api/posts/:id", function(req, res) {
-    db.Post.findOne({
-      where: {
-        id: req.params.id
-      }
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
-
-  // POST route for saving a new post
-  app.post("/api/posts", function(req, res) {
+  app.post("/api/signup", function(req, res) {
     console.log(req.body);
-    db.Post.create({
-      title: req.body.title,
-      body: req.body.body,
-      category: req.body.category
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
+    db.User.create({
+      email: req.body.email,
+      password: req.body.password
+    }).then(function() {
+      res.redirect(307, "/api/login");
+    }).catch(function(err) {
+      console.log(err);
+      res.json(err);
+    });
   });
 
-  // DELETE route for deleting posts
-  app.delete("/api/posts/:id", function(req, res) {
-    db.Post.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
+  app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
   });
 
-  // PUT route for updating posts
-  app.put("/api/posts", function(req, res) {
-    db.Post.update(req.body,
-      {
-        where: {
-          id: req.body.id
-        }
-      })
-      .then(function(dbPost) {
-        res.json(dbPost);
+  app.get("/api/user_data", function(req, res) {
+    if (!req.user) {
+      res.json({});
+    }
+    else {
+      res.json({
+        email: req.user.email,
+        id: req.user.id
       });
+    }
   });
+
 };
