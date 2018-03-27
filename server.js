fd36de135809717d454ae771d356f91005ab2aba
unknown <gw-danmace@hotmail.com>
2018-03-27 10:16:02 -0400

    minor omission in package.json corrected

--- server.js
+++ server.js
@@ -3,29 +3,6 @@ var bodyParser = require("body-parser");
 var session = require("express-session");
 var passport = require("./config/passport");
 
-
-
-var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
-
-
-
-
-
-
-
-
-
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
 
