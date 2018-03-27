463732bc761c09e51b971bdb41a4210ed7e04a2c
unknown <gw-danmace@hotmail.com>
2018-03-27 11:46:06 -0400

    name change with index.handlebars

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var passport = require("./config/passport");
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
 
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
